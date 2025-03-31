ad4320cb307b6001b335aa9dcb4b665a18b1b8e5
Matias <matias.rojas0852@protonmail.com>
2025-03-30 21:12:34 -0300

    Creacion del django admin

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Autor, Libro
 
-# Register your models here.
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "nacionalidad")
+    search_fields = ("nombre", "nacionalidad")
+
+
+@admin.register(Libro)
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ("titulo", "autor", "genero", "fecha_publicacion")
+    list_filter = ("genero", "fecha_publicacion")
+    search_fields = ("titulo", "autor__nombre")
+    date_hierarchy = "fecha_publicacion"
