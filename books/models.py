6b53f29d5d8b5bef125a1195a7a1de179ec848c1
Matias <matias.rojas0852@protonmail.com>
2025-03-30 21:04:56 -0300

    Creacion de modelos

--- books/models.py
+++ books/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    nacionalidad = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=200)
+    fecha_publicacion = models.DateField()
+    genero = models.CharField(max_length=50)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE, related_name='libros')
+
+    def __str__(self):
+        return self.titulo
